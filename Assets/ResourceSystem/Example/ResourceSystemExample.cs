952d85a0a7fdfe7dcc6ad18bdb60c3b38d396de1
Khanh <khanhwizard>
2025-09-28 12:55:42 +0700

    added example

---
+++ Assets/ResourceSystem/Example/ResourceSystemExample.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace ResourceSystem.Example
+{
+    [DefaultExecutionOrder(-100)]
+    public class ResourceSystemExample : MonoBehaviour
+    {
+        [Serializable]
+        public class ExampleData
+        {
+            public int Id;
+            public int Amount;
+            public bool IsItemType;
+            public string Name;
+            public string Desc;
+            public int Rarity;
+            public Sprite Icon;
+        }
+
+        [Serializable]
+        public class RewardData
+        {
+            [Tooltip("Resource ID, cannot change in play mode")]
+            public int Id;
+            public int Amount;
+            public GameObject View;
+        }
+
+        public TextMeshProUGUI ResourceNameText;
+        public List<ExampleData> DataList = new List<ExampleData>();
+
+        [Header("Owned Item")]
+        public List<RewardData> InitialOwnedItems = new List<RewardData>();
+        public ResourcesListView OwnedItemListView;
+
+
+        [Header("UI")]
+        public Button _addButton;
+        public Button _resetAllRewardClaimButton;
+        public TMP_InputField _idInput;
+        public TMP_InputField _amountInput;
+
+        private List<ResourceUsageData> _ownedItemCache = new List<ResourceUsageData>();
+        private bool _ownedItemDirty = true;
+
+        public OwnedResourceManager OwnedResourceManager => OwnedResourceManager.Instance;
+        public ResourceManager ResourceManager => ResourceManager.Instance;
+
+
+        void Start()
+        {
+            foreach (ExampleData data in DataList)
+            {
+                if (data.IsItemType)
+                {
+                    ItemResourceData itemData = new ItemResourceData(data.Id, data.Name, data.Desc, data.Icon, data.Rarity);
+                    ResourceManager.Instance.AddResourceData(itemData);
+                }
+                else
+                {
+                    CurrencyResourceData currencyData = new CurrencyResourceData(data.Id, data.Name, data.Desc, data.Icon, data.Rarity, 0, 0, 0, 0);
+                    ResourceManager.Instance.AddResourceData(currencyData);
+                }
+
+                ReceiveResource(data.Id, data.Amount);
+            }
+
+            _addButton.onClick.AddListener(OnAddButtonClicked);
+            _resetAllRewardClaimButton.onClick.AddListener(ResetAllRewardClaim);
+
+            foreach (var reward in InitialOwnedItems)
+            {
+                ReceiveResource(reward.Id, reward.Amount);
+            }
+
+            foreach (var ownedItem in OwnedResourceManager.GetAllOwnedItems())
+            {
+                _ownedItemCache.Add(new ResourceUsageData(ownedItem.Data, 1));
+            }
+
+            OwnedResourceManager.OnOwnedResourceChanged += (data, changedValue) =>
+            {
+                if (data is IItemType)
+                {
+                    if (changedValue > 0)
+                    {
+                        for (int i = 0; i < changedValue; i++)
+                        {
+                            _ownedItemCache.Add(new ResourceUsageData(data, 1));
+                        }
+                    }
+                    else if (changedValue < 0)
+                    {
+                        int removeCount = -changedValue;
+                        for (int i = _ownedItemCache.Count - 1; i >= 0 && removeCount > 0; i--)
+                        {
+                            if (_ownedItemCache[i].ResourceData.Id == data.Id)
+                            {
+                                _ownedItemCache.RemoveAt(i);
+                                removeCount--;
+                            }
+                        }
+                    }
+
+                    _ownedItemDirty = true;
+                }
+            };
+        }
+
+        void Update()
+        {
+            // UpdateShopUi();
+
+            if (!string.IsNullOrEmpty(_idInput.text) && int.TryParse(_idInput.text, out int id))
+            {
+                ResourceData resourceData = ResourceManager.Instance.GetResourceData(id);
+                if (resourceData != null)
+                {
+                    ResourceNameText.text = resourceData is IHasName hasName ? hasName.Name : "No Name";
+                }
+                else
+                {
+                    ResourceNameText.text = "Not Found";
+                }
+            }
+            else
+            {
+                ResourceNameText.text = "";
+            }
+
+            if (_ownedItemDirty)
+            {
+                _ownedItemDirty = false;
+                OwnedItemListView.UpdateInfo(_ownedItemCache);
+            }
+        }
+
+        public void OnAddButtonClicked()
+        {
+            if (int.TryParse(_amountInput.text, out int amount))
+            {
+                if (int.TryParse(_idInput.text, out int id))
+                {
+                    ReceiveResource(id, amount);
+                }
+                else
+                {
+                    Debug.LogError("Invalid ID input");
+                }
+            }
+            else
+            {
+                Debug.LogError("Invalid amount input");
+            }
+        }
+
+        public void ReceiveResource(int id, int amount)
+        {
+            ResourceData resourceData = ResourceManager.Instance.GetResourceData(id);
+            OwnedResourceManager.AddOwnedResource(resourceData, amount);
+        }
+
+        public void ResetAllRewardClaim()
+        {
+            foreach (var e in FindObjectsByType<ClaimResourceView>(FindObjectsSortMode.None))
+            {
+                e.Renew();
+            }
+        }
+    }
+}
---
+++ Assets/ResourceSystem/Example/ShowResourceListProviderExample.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResourceSystem.Example
+{
+    public class ShowResourceListProviderExample : MonoBehaviour, IResourceListViewProvider
+    {
+        public GameObject Prefab;
+        public Canvas Canvas;
+
+        private void Start()
+        {
+            RewardDataExtensions.ListViewProvider = this;
+        }
+
+        public void ShowList(IEnumerable<ResourceUsageData> datas)
+        {
+            GameObject instance = Instantiate(Prefab, Canvas.transform);
+            instance.transform.localPosition = Vector3.zero;
+            instance.transform.SetAsLastSibling();
+            instance.GetComponent<ResourcesListView>().UpdateInfo(datas);
+        }
+    }
+}
